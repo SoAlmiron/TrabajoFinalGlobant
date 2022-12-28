16b709963465423c8b7cac1c8d0bf168adbff63f
JMRoco <jmroco.globant@gmail.com>
2022-12-28 15:27:32 -0300

    Name package changed

---
+++ src/test/java/com/example/proyectoFinal1/ProyectoyFinal1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoFinal1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoyFinal1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
