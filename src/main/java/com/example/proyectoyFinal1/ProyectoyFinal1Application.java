8595a621469ad02597527ea683e97b31aec56708
Ruben Bl <ruben.h.blanco1@gmail.com>
2022-12-28 14:37:33 -0300

    -

---
+++ src/main/java/com/example/proyectoyFinal1/ProyectoyFinal1Application.java
@@ -0,0 +1,13 @@
+package com.example.proyectoyFinal1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoyFinal1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoyFinal1Application.class, args);
+	}
+
+}
