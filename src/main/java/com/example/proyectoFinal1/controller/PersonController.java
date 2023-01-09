f91da2ea87f851a8a1074111e656c4370e823d47
SoAlmiron <soofi.2636@gmail.com>
2023-01-08 22:38:19 -0300

    I update the method to delete a person

--- src/main/java/com/example/proyectoFinal1/controller/PersonController.java
+++ src/main/java/com/example/proyectoFinal1/controller/PersonController.java
@@ -4,10 +4,9 @@ package com.example.proyectoFinal1.controller;
 import com.example.proyectoFinal1.model.Person;
 import com.example.proyectoFinal1.service.PersonService;
 import jakarta.persistence.GeneratedValue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,16 +14,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/persons")
 public class PersonController {
-
-    private final PersonService personService;
-
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
+    @Autowired
+    private PersonService personService;
 
     @GetMapping
     public List<Person> getAllPersons(){
         return personService.getAllPersons();
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public void deletePerson(@PathVariable("id") Long id) {
+        personService.deletePerson(id);
     }
 
 
--- src/main/java/com/example/proyectoFinal1/service/PersonService.java
+++ src/main/java/com/example/proyectoFinal1/service/PersonService.java
@@ -2,13 +2,15 @@ package com.example.proyectoFinal1.service;
 
 import com.example.proyectoFinal1.model.Person;
 import com.example.proyectoFinal1.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class PersonService {
 
-    private final PersonRepository repository;
+    @Autowired
+    private PersonRepository repository;
 
     public PersonService(PersonRepository repository) {
         this.repository = repository;
@@ -16,5 +18,13 @@ public class PersonService {
 
     public List<Person> getAllPersons() {
         return repository.findAll();
+    }
+
+    public void deletePerson(Long id) {
+        /*boolean exist = repository.existsById(id);
+        if (!exist){
+            throw new RuntimeException("The person with id" + id + "does not exist");
+        }*/
+        repository.deleteById(id);
     }
 }
