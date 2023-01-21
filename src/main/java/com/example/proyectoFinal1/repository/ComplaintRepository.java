a328e24071afeaf714072d6cee9b489b8ccf5b8c
JMRoco <jmroco.globant@gmail.com>
2023-01-20 23:17:29 -0300

    -Complaint basic structure created -getAllComplaint GET method created

--- src/main/java/com/example/proyectoFinal1/repository/ComplaintRepository.java
+++ src/main/java/com/example/proyectoFinal1/repository/ComplaintRepository.java
@@ -1,4 +1,13 @@
 package com.example.proyectoFinal1.repository;
 
-public interface ComplaintRepository {
+import com.example.proyectoFinal1.model.Complaint;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
+    List<Complaint> complaintRepository = new ArrayList<>();
 }
--- src/main/java/com/example/proyectoFinal1/service/ComplaintService.java
+++ src/main/java/com/example/proyectoFinal1/service/ComplaintService.java
@@ -1,4 +1,21 @@
 package com.example.proyectoFinal1.service;
 
+import com.example.proyectoFinal1.model.Complaint;
+import com.example.proyectoFinal1.repository.ComplaintRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ComplaintService {
+
+    private final ComplaintRepository repository;
+
+    public ComplaintService(ComplaintRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Complaint> getAllComplaints() {
+        return repository.findAll();
+    }
 }
