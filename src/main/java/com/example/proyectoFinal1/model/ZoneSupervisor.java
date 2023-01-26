751a5e8091ffbeae82d9e14b14c45219eb1ef468
JMRoco <jmroco.globant@gmail.com>
2023-01-25 23:51:26 -0300

    id, name attributes deleted constructor deleted

--- src/main/java/com/example/proyectoFinal1/model/ZoneSupervisor.java
+++ src/main/java/com/example/proyectoFinal1/model/ZoneSupervisor.java
@@ -1,13 +1,9 @@
 package com.example.proyectoFinal1.model;
-import com.example.proyectoFinal1.model.RecyclingZone;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.beans.ConstructorProperties;
-import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
@@ -16,22 +12,9 @@ import java.time.LocalDate;
 
 @Table(name = "zonesupervisors")
 public class ZoneSupervisor extends Person{
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "name")
-    private String name;
-
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "recyclingZone_id")
     @JsonBackReference //IMPORTANTE
     private RecyclingZone recyclingZone;
-
-    public ZoneSupervisor(String name, RecyclingZone recyclingZone) {
-        super(name);
-        this.recyclingZone = recyclingZone;
-    }
 
 }
